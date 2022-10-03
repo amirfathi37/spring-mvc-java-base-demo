c03c7826de42e0ce18dce7169dfd1a32f6c04391
AMIR FATHI <fathiamir37@gmail.com>
2022-10-03 18:04:15 +0330

    Add files via upload

---
+++ src/main/java/com/fathi/config/AppIntilizer.java
@@ -0,0 +1,17 @@
+package com.fathi.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppIntilizer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
